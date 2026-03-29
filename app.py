9807cbf762789b36f70f981e45a5af22b624a6ec
pythogh <139380324+pythogh@users.noreply.github.com>
2026-03-29 01:50:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,13 @@
 import requests
-import time
+import json
 
-URL = "https://api.hyperliquid.xyz/info"
+url = "https://api.hyperliquid.xyz/info"
 
-# coins HIP3
-COINS = [
-    "xyz:NVDA",
-    "drm:NVDA",
-    "kin:NVDA",
+payload = {
+    "type": "l2Book",
+    "coin": "xyz:NVDA"
+}
 
-    "xyz:TSLA",
-    "drm:TSLA",
-    "kin:TSLA",
+r = requests.post(url, json=payload)
 
-    "xyz:GOOGL",
-    "drm:GOOGL",
-    "kin:GOOGL",
-]
-
-
-def get_mid(coin):
-
-    payload = {
-        "type": "l2Book",
-        "coin": coin
-    }
-
-    try:
-        r = requests.post(URL, json=payload, timeout=5)
-        data = r.json()
-
-        bids = data["levels"][0]
-        asks = data["levels"][1]
-
-        bid = float(bids[0]["px"])
-        ask = float(asks[0]["px"])
-
-        mid = (bid + ask) / 2
-
-        return mid
-
-    except Exception as e:
-        print("error", coin, e)
-        return None
-
-
-while True:
-
-    prices = {}
-
-    for coin in COINS:
-
-        mid = get_mid(coin)
-
-        if mid:
-            prices[coin] = mid
-
-    print("\nPrices:")
-    for k, v in prices.items():
-        print(k, v)
-
-    time.sleep(2)
+print(r.json())
